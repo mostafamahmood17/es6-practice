a374ef6ec58d71b28ac90a041f96c87750a6dd5a
mostafamahmood17 <taat.heritage@gmail.com>
2020-08-05 17:04:58 -0700

    class2

--- class2.js
+++ class2.js
@@ -1,16 +1,15 @@
-class phone{
+class Phone{
     constructor(color, model, city){
         this.color = color;
         this.model = model;
         this.warehouse = city;
 
-
     }   
 
 }
 
-const iphone = new phone("Black", "iphone 11", "Los Angeles");
-const samsung = new phone("Silver", "s20", "Dallas" );
-const onePlus = new phone("Black", "0ne 8", "Miami");
+const iphone = new Phone("Black", "iphone 11", "Los Angeles");
+const samsung = new Phone("Silver", "s20", "Dallas" );
+const onePlus = new Phone("Black", "0ne 8", "Miami");
 
 console.log(onePlus);
